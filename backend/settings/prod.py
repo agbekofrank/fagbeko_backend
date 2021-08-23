ea03b8c3e1b8deeb61062b8be3828ba58fa5a469
frankagbeko <frank.agbeko@amalitech.org>
2021-08-23 09:26:08 +0000

    Group all schemas

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -7,6 +7,6 @@ environ.Env.read_env()
 
 SECRET_KEY = env.str('DJANGO_SECRET_KEY', '%p8#dko(q2l2d+9-k(f)6w-1p$(*3*y2v#+^ebjxka@og*oocd')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['portfolioag.herokuapp.com']
